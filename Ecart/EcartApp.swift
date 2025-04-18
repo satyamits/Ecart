0c8bcfab1ec32116ed0ed303f7540c47eff32ac5
Mohit Kumar Singh <mohit.kumar@nickelfox.com>
2025-04-18 17:59:59 +0530

    Initial Commit

---
+++ Ecart/EcartApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcartApp.swift
+//  Ecart
+//
+//  Created by Satyam Singh on 18/04/25.
+//
+
+import SwiftUI
+
+@main
+struct EcartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
