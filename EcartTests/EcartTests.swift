66143afd8d6cb9646a028fd4e2b4eccd94abb6cd
Mohit Kumar Singh <mohit.kumar@nickelfox.com>
2025-04-18 17:59:59 +0530

    Initial Commit

---
+++ EcartTests/EcartTests.swift
@@ -0,0 +1,17 @@
+//
+//  EcartTests.swift
+//  EcartTests
+//
+//  Created by Satyam Singh on 18/04/25.
+//
+
+import Testing
+@testable import Ecart
+
+struct EcartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
